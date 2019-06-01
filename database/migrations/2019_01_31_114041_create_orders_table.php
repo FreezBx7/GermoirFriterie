05916bc35cc7f09a23689cb69a1213fa57182e0f
Cyriaque <cyriaque.hockey@hotmail.fr>
2019-06-01 16:10:35 +0200

    test(vardump)

---
+++ database/migrations/2019_01_31_114041_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('name');
+            $table->string('comments', 50);
+            $table->integer('quantity');
+
+            $table->string('nameBakings', 50);
+            $table->foreign('nameBakings')->references('name')->on('bakings')->onUpdate('cascade')
+                ->onDelete('cascade');
+
+
+            $table->string('namePresta', 50);
+            $table->foreign('namePresta')->references('name')->on('prestations')->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->float('totalPrice');
+
+            $table->timestamps();
+            var_dump($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
