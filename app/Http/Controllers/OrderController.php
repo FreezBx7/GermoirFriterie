120df211f523f5a64fc096b8bd0f9735cf3449b5
Cyriaque <cyriaque.hockey@hotmail.fr>
2019-06-07 00:03:31 +0200

    order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,7 +14,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $result = unserialize($order->all());
+        $result = unserialize(Order::all);
         return $result;
     }
 
