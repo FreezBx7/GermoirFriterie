77d4f2979256960c07e933bd129088e478bcfd12
Cyriaque <cyriaque.hockey@hotmail.fr>
2019-06-06 23:50:16 +0200

    fix(db):order

--- database/migrations/2019_01_31_114041_create_orders_table.php
+++ database/migrations/2019_01_31_114041_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('json', 6000000);
+            $table->text('json');
             $table->timestamps();
         });
     }
